3369656d0b368183e96f6eecd033b93f74763d2c
Hexirp <Hexirp@users.noreply.github.com>
2017-09-17 11:44:33 +0900

    Add dependecies

--- src/main/scala/io/github/hexirp/akariki/Main.scala
+++ src/main/scala/io/github/hexirp/akariki/Main.scala
@@ -3,7 +3,7 @@ package io.github.hexirp.akariki
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 
-@Mod(modid = "akariki", modLanguage = "scala")
+@Mod(modid = "akariki", dependencies = "after:forge@[14.22.1.2478,)" , modLanguage = "scala")
 object Main {
   @Mod.EventHandler
   def init(e : FMLInitializationEvent) : Unit = ()
