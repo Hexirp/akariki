444a1ff3f79527a1d3b2d4129b9f4001a33e2500
Hexirp <Hexirp@users.noreply.github.com>
2017-09-17 17:43:56 +0900

    Add print test

--- src/main/scala/io/github/hexirp/akariki/Boot.java
+++ src/main/scala/io/github/hexirp/akariki/Boot.java
@@ -1,45 +1,34 @@
 package io.github.hexirp.akariki;
 
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.item.crafting.IRecipe;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = "akariki", dependencies = "after:forge@[14.22.1.2478,)")
+@Mod.EventBusSubscriber
 public class Boot {
-    public static final Item sample = new Item()
-            .setRegistryName("akariki:sample")
-            .setCreativeTab(CreativeTabs.MISC)
-            .setUnlocalizedName("sample");
-
-    @SubscribeEvent
-    public void regItem(RegistryEvent.Register<Item> e) {
-        e.getRegistry().register(sample);
-    }
-
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent e) {
-        MinecraftForge.EVENT_BUS.register(Boot.class);
-
-        if (e.getSide() == Side.CLIENT) {
-            ModelLoader.setCustomModelResourceLocation(sample, 0,
-                    new ModelResourceLocation("akariki:sample", "inventory"));
-        }
+        System.out.print("preInit");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent e) {
+        System.out.print("init");
         Main.init(e);
     }
 
     @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent e) {}
+    public void postInit(FMLPostInitializationEvent e) {
+        System.out.print("postInit");
+    }
+
+    @SubscribeEvent
+    public void regRecipe(RegistryEvent.Register<IRecipe> e) {
+        System.out.print("regRecipe");
+    }
 }
