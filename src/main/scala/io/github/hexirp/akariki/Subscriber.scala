1aa0c53c7add9010e7ed6aede9a59e9ac92f352c
Hexirp <Hexirp@users.noreply.github.com>
2017-09-18 20:05:38 +0900

    Add sample item

--- src/main/scala/io/github/hexirp/akariki/Subscriber.scala
+++ src/main/scala/io/github/hexirp/akariki/Subscriber.scala
@@ -1,15 +1,26 @@
 package io.github.hexirp.akariki
 
 import net.minecraft.block.Block
+import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.Item
 import net.minecraft.item.crafting.IRecipe
+import net.minecraft.util.ResourceLocation
 import net.minecraftforge.event.RegistryEvent
 import org.apache.logging.log4j.Logger
 
 class Subscriber(log : Logger) {
+  val sample : Item = new Item()
+    .setRegistryName(new ResourceLocation("akariki", "sample"))
+    .setCreativeTab(CreativeTabs.MISC)
+    .setUnlocalizedName("sample")
+
   def regRecipe(e : RegistryEvent.Register[IRecipe]) = log.info("regRecipe")
 
-  def regItem(e : RegistryEvent.Register[Item]) = log.info("regItem")
+  def regItem(e : RegistryEvent.Register[Item]) = {
+    log.info("regItem")
+
+    e.getRegistry.register(sample)
+  }
 
   def regBlock(e : RegistryEvent.Register[Block]) = log.info("regBlock")
 }
