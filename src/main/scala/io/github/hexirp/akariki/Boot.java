9be93cef6424ab7b7c69ac11a6860b783d92b7d0
Hexirp <Hexirp@users.noreply.github.com>
2017-09-18 10:48:58 +0900

    Add Subscriber
    I was suffering from the fact that the RegistryEvent was not fired, but because
    the method is not static.

--- src/main/scala/io/github/hexirp/akariki/Boot.java
+++ src/main/scala/io/github/hexirp/akariki/Boot.java
@@ -1,24 +1,22 @@
 package io.github.hexirp.akariki;
 
-import net.minecraft.item.Item;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = "akariki", version = "0.0.0", dependencies = "after:forge@[14.22.1.2478,)")
-@Mod.EventBusSubscriber
 public class Boot {
-    public Logger log = LogManager.getFormatterLogger("akariki");
+    private final Logger log = LogManager.getFormatterLogger("akariki");
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent e) {
+        MinecraftForge.EVENT_BUS.register(new Subscriber(log));
+
         log.info("preInit");
     }
 
@@ -31,20 +29,5 @@ public class Boot {
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent e) {
         log.info("postInit");
-    }
-
-    @SubscribeEvent
-    public void regRecipe(RegistryEvent.Register<IRecipe> e) {
-        throw new NullPointerException("regRecipe");
-    }
-
-    @SubscribeEvent
-    public void regItem(RegistryEvent.Register<Item> e) {
-        throw new NullPointerException("regItem");
-    }
-
-    @SubscribeEvent
-    public void regBlock(RegistryEvent.Register<net.minecraft.block.Block> e) {
-        throw new NullPointerException("regBlock");
     }
 }
---
+++ src/main/scala/io/github/hexirp/akariki/Subscriber.java
@@ -0,0 +1,30 @@
+package io.github.hexirp.akariki;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.Logger;
+
+public class Subscriber {
+    private final Logger log;
+
+    public Subscriber(Logger log) {
+        this.log = log;
+    }
+
+    @SubscribeEvent
+    public void regRecipe(RegistryEvent.Register<IRecipe> e) {
+        log.info("regRecipe");
+    }
+
+    @SubscribeEvent
+    public void regItem(RegistryEvent.Register<Item> e) {
+        log.info("regItem");
+    }
+
+    @SubscribeEvent
+    public void regBlock(RegistryEvent.Register<net.minecraft.block.Block> e) {
+        log.info("regBlock");
+    }
+}
