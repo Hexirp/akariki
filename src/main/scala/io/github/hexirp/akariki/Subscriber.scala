94a5d3204838a5255511ada5633a9d49c47f2e20
Hexirp <Hexirp@users.noreply.github.com>
2017-09-23 17:02:17 +0900

    Update to use ItemList at Subscriber
    Refs:
    f4e9cd4b1e6e972329a7b2564f8d01f037a71ca0
    61df4c769e8c3f282a88cc7ace1c36bf97b71cda

--- src/main/scala/io/github/hexirp/akariki/Subscriber.scala
+++ src/main/scala/io/github/hexirp/akariki/Subscriber.scala
@@ -1,24 +1,17 @@
 package io.github.hexirp.akariki
 
 import net.minecraft.block.Block
-import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.item.Item
 import net.minecraft.item.crafting.IRecipe
-import net.minecraft.util.ResourceLocation
 import net.minecraftforge.event.RegistryEvent
 import org.apache.logging.log4j.Logger
 
 class Subscriber(log : Logger, items : ItemList) {
-  val sample : Item = new Item()
-    .setRegistryName(new ResourceLocation(Metadata.modid, "sample"))
-    .setCreativeTab(CreativeTabs.MISC)
-    .setUnlocalizedName("sample")
-
   def regRecipe(event : RegistryEvent.Register[IRecipe]) : Unit = log.info("regRecipe")
 
   def regItem(event : RegistryEvent.Register[Item]) : Unit = {
     log.info("regItem")
-    event.getRegistry.register(sample)
+    event.getRegistry.register(items.sample)
   }
 
   def regBlock(event : RegistryEvent.Register[Block]) : Unit = log.info("regBlock")
