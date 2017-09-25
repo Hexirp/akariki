75cbaefae782395f29137c97acce2becae576a4b
Hexirp <Hexirp@users.noreply.github.com>
2017-09-25 20:36:20 +0900

    Update internal version
    net.minecraftforge.fml.common.Mod.version follows
    https://cwiki.apache.org/confluence/display/MAVENOLD/Versioning

--- src/main/scala/io/github/hexirp/akariki/Metadata.java
+++ src/main/scala/io/github/hexirp/akariki/Metadata.java
@@ -3,7 +3,7 @@ package io.github.hexirp.akariki;
 public class Metadata {
     public static final String modid = "akariki";
 
-    public static final String version = "0.0.0-alpha1";
+    public static final String version = "0.0.0-alpha,1";
 
     public static final String dependencies = "after:forge@[14.22.1.2478,)";
 }
