b6a0f7a9a4963794970a190db60b0e1d2a9a4a35
Hexirp <Hexirp@users.noreply.github.com>
2017-10-23 11:53:53 +0900

    Add Blocks.regBlock

--- src/main/scala/io/github/hexirp/akariki/Blocks.scala
+++ src/main/scala/io/github/hexirp/akariki/Blocks.scala
@@ -4,10 +4,15 @@ import net.minecraft.block.Block
 import net.minecraft.block.material.Material
 import net.minecraft.creativetab.CreativeTabs
 import net.minecraft.util.ResourceLocation
+import net.minecraftforge.event.RegistryEvent
 
 class Blocks {
   var silver_ore : Block = new Block(Material.ROCK)
     .setRegistryName(new ResourceLocation(Metadata.modid, "silver_ore"))
     .setCreativeTab(CreativeTabs.BUILDING_BLOCKS)
     .setUnlocalizedName("silver_ore")
+
+  def regBlock(event : RegistryEvent.Register[Block]) = {
+    event.getRegistry.register(silver_ore)
+  }
 }
