5ece54a48a58e243ef6105ad25667bdc5e228ebc
Hexirp <Hexirp@users.noreply.github.com>
2017-12-02 21:22:22 +0900

    Change the name of the variable more simply

--- src/main/scala/io/github/hexirp/akariki/Boot.java
+++ src/main/scala/io/github/hexirp/akariki/Boot.java
@@ -11,23 +11,23 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = Metadata.modid, version = Metadata.version, dependencies = Metadata.dependencies)
 public class Boot {
     private final Logger log = LogManager.getFormatterLogger(Metadata.modid);
-    private final Initializer initer = new Initializer(log);
+    private final Initializer init = new Initializer(log);
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         Blocks blocks = new Blocks();
         Items items = new Items(blocks);
         new Post(log, items, blocks).join();
-        initer.preInit(event);
+        init.preInit(event);
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        initer.init(event);
+        init.init(event);
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        initer.postInit(event);
+        init.postInit(event);
     }
 }
