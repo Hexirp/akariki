ad4141237fd8796d20e2ecbb144e4e60360b5e44
Hexirp <Hexirp@users.noreply.github.com>
2017-12-15 22:12:05 +0900

    Update Metadata to change it to a helper class

--- src/main/scala/io/github/hexirp/akariki/Metadata.java
+++ src/main/scala/io/github/hexirp/akariki/Metadata.java
@@ -1,6 +1,16 @@
 package io.github.hexirp.akariki;
 
+import net.minecraft.util.ResourceLocation;
+
 /**
  * {@link Metadata} is data of akariki.
  */
-public final class Metadata {}
+public class Metadata {
+    public String MOD_ID() {
+        return Boot.MOD_ID;
+    }
+
+    public ResourceLocation resourceLocation(String name) {
+        return new ResourceLocation(MOD_ID(), name);
+    }
+}
