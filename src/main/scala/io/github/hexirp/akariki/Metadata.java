619928030e086420808e023cf79a203e31a5112d
Hexirp <Hexirp@users.noreply.github.com>
2017-12-16 13:41:59 +0900

    Update Metadata for logging

--- src/main/scala/io/github/hexirp/akariki/Metadata.java
+++ src/main/scala/io/github/hexirp/akariki/Metadata.java
@@ -2,15 +2,29 @@ package io.github.hexirp.akariki;
 
 import net.minecraft.util.ResourceLocation;
 
+import org.apache.logging.log4j.core.Logger;
+
 /**
  * {@link Metadata} is data of akariki.
  */
 public class Metadata {
-    public String MOD_ID() {
-        return Boot.MOD_ID;
+    private final String id;
+    private final Logger log;
+
+    public Metadata(String id, Logger log) {
+        this.id = id;
+        this.log = log;
     }
 
-    public ResourceLocation resourceLocation(String name) {
-        return new ResourceLocation(MOD_ID(), name);
+    public String id() {
+        return id;
+    }
+
+    public ResourceLocation newResourceLocation(String name) {
+        return new ResourceLocation(id, name);
+    }
+
+    public void info(String message) {
+        log.info(message);
     }
 }
