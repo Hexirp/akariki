76abe0ba0e05314148eedcc3230944ec4bc16ae4
Hexirp <Hexirp@users.noreply.github.com>
2018-01-05 20:38:53 +0900

    Bump version for #16

--- src/main/scala/io/github/hexirp/akariki/Boot.java
+++ src/main/scala/io/github/hexirp/akariki/Boot.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 /**
  * {@link Boot} connects Forge's initialization events to {@link Initializer} and works as a main class of akariki.
  */
-@Mod(modid = Boot.MOD_ID, version = "0.0.0-alpha-1", dependencies = "required-after:forge@[14.23.1.2555,)")
+@Mod(modid = Boot.MOD_ID, version = "0.1.0-alpha-1", dependencies = "required-after:forge@[14.23.1.2555,)")
 public class Boot {
     static final String MOD_ID = "akariki";
 
