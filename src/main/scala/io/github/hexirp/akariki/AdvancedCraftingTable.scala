acfb258fd2c118385fd27867bd7c20675f76b617
Hexirp <Hexirp@users.noreply.github.com>
2018-12-08 15:23:29 +0900

    Update: AdvancedCraftingTable の物性を

--- src/main/scala/io/github/hexirp/akariki/AdvancedCraftingTable.scala
+++ src/main/scala/io/github/hexirp/akariki/AdvancedCraftingTable.scala
@@ -4,9 +4,11 @@ import net.minecraft.block.Block
 import net.minecraft.block.material.Material
 import net.minecraft.creativetab.CreativeTabs
 
-class AdvancedCraftingTable(metadata: Metadata) extends Block(Material.ROCK) {
+class AdvancedCraftingTable(metadata: Metadata) extends Block(Material.WOOD) {
   this
     .setRegistryName(metadata.newResourceLocation("advanced_crafting_table"))
     .setCreativeTab(CreativeTabs.DECORATIONS)
     .setUnlocalizedName("advanced_crafting_table")
+    .setHardness(3.0f)
+    .setResistance(15.0f)
 }
