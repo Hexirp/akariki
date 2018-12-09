043488bd3f4c35407f9a13a805edaaa605219ea6
Hexirp <Hexirp@users.noreply.github.com>
2018-12-09 22:31:26 +0900

    Fix: 末尾の句点を

--- src/main/scala/io/github/hexirp/akariki/PrismarineFence.scala
+++ src/main/scala/io/github/hexirp/akariki/PrismarineFence.scala
@@ -4,7 +4,7 @@ import net.minecraft.block.BlockFence
 import net.minecraft.block.material.{MapColor, Material}
 import net.minecraft.creativetab.CreativeTabs
 
-/** [[PrismarineFence]] はプリズマリンから出来たフェンスである、
+/** [[PrismarineFence]] はプリズマリンから出来たフェンスである。
   *
   * Minecraf 1.13.x への対応の時に、このアイテムは、水を通さない特殊なフェンスになることが予定されている。
   * そのため、特別扱いする必要があることが予測される。そのため Items の中から分けた。
